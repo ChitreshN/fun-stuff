0efe31e151341b9c34a3291e188bd10b49a43332
ChitreshN <chitreshnarra@gmail.com>
2023-10-05 01:23:08 +0530

    Setup framework of chapter 17

--- compiler.c
+++ compiler.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source){
+bool compile(const char *source, Chunk* chunk){
     initScanner(source);
-    int line = -1;
-    for (;;){
-        Token token = scanToken();
-        if (token.line != line){
-            printf("%4d", token.line);
-            line = token.line;
-        }
-        else {
-            printf("    | ");
-        }
-        printf("%2d '%.*s'\n",token.type, token.length, token.start);
-
-        if (token.type == TOKEN_EOF) break;
-    }
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
--- vm.c
+++ vm.c
@@ -80,6 +80,19 @@ static IntrepretResult run(){
 }
 
 IntrepretResult interpret(const char* source){
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)){
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    IntrepretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
